f4893f587bf688c128b93a8daefbd3b3dfb2d813
fujiwarayuusuke <hujiwara.yuusuke.28n@st.kyoto-u.ac.jp>
2024-10-10 12:46:44 +0900

    parserの修正に伴い関数の返り値を変更

--- myproject/lib/output.ml
+++ myproject/lib/output.ml
@@ -1,14 +1,13 @@
 open Eval
 open Typing
 
-(*バッチインタプリタの場合のevalprint*)
 let read_eval_print env tyenv inchannel =
   (* 入力からバッファを生成 *)
   let buffer = Lexing.from_channel inchannel in
-  let ast = Parser.toplevel Lexer.main buffer in
-  let _ = eval_main env ast in
+  let (_, exp_ast) = Parser.toplevel Lexer.main buffer in
+  let _ = eval_main env exp_ast in
   (* let (_, ty) = ty_exp tyenv ast in *)
-  print_ast ast
+  print_ast exp_ast
 
 let initial_env =
   Environment.empty
