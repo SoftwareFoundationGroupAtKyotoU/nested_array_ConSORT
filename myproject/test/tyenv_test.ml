1d362622b0a703de7d5a9f2d4d7e8af2a02b22e3
fujiwarayuusuke <hujiwara.yuusuke.28n@st.kyoto-u.ac.jp>
2024-10-12 12:07:18 +0900

    テストケースの修正

--- myproject/test/tyenv_test.ml
+++ myproject/test/tyenv_test.ml
@@ -1,18 +1,17 @@
 open OUnit2
 open Myproject.Syntax
 open Myproject.Typing
-open Myproject.Environment
 (* 型環境のテスト *)
 let test_empty _ =
-  assert_equal empty (tyenv_test (ILit 1))
+  assert_equal [] (tyenv_test (ILit 1))
 
 let test_tyint _ =
-  assert_equal empty (tyenv_test (LetBindExp( "n", ILit 1 , ILit 1 )))
+  assert_equal [("m", SInt)] (tyenv_test (LetBindExp( "m", ILit 1 , ILit 1 )))
 
-(* let test_alloc _ = 
-  assert_equal (SRef SInt) (ty_test (LetBinOpExp( "x", BinOp( Plus , ILit 3 , ILit 5 ) , LetAllocExp( "y", Var "x" , SRef ( SInt ) , Var "y" ) ) ))
+let test_int_ref _ = 
+  assert_equal [("x", SRef SInt);("n", SInt)] (tyenv_test (LetBindExp( "n", ILit 1 , LetAllocExp( "x", Var "n" , SRef ( SInt ) , PreSEMIExpr ( Assign ( "x", "n" ) , PreSEMIExpr ( Assert (BinOp( Eq , Deref ( Var "x" ) , ILit 1 ) ) , ILit 1 ) ) ) )))
 
-let test_assign _ = 
+(* let test_assign _ = 
   assert_equal SInt (ty_test ( LetBindExp( "z", ILit 3 , LetAllocExp( "x", Var "z" , SRef ( SInt ) , PreSEMIExpr ( Assign ( "x", "z" ) , LetDerefExp ( "y", Var "x" , Var "y" ) ) ) ) ))
 
 let test_alias _ =
@@ -22,8 +21,8 @@ let suite =
   "Tyenv Test" >::: [
     "test_empty" >:: test_empty;
     "test_tyint" >:: test_tyint;
-    (* "test_alloc" >:: test_alloc;
-    "test_assign" >:: test_assign;
+    "test_int_ref" >:: test_int_ref;
+    (* "test_assign" >:: test_assign;
     "test_alias" >:: test_alias; *)
   ]
 
