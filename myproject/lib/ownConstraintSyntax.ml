3edfcff8a323f00c32f305ad041e211e2e596e86
fujiwarayuusuke <hujiwara.yuusuke.28n@st.kyoto-u.ac.jp>
2024-11-12 18:41:58 +0900

    所有権制約のための文法を定義

---
+++ myproject/lib/ownConstraintSyntax.ml
@@ -0,0 +1,24 @@
+open Syntax
+
+(* 関数内の位置を表す型，イメージ的にはプログラムの書かれた行 *)
+type pos = int
+
+(** AST with position information used for ownership inference *)
+type constr = 
+  | CIf of exp * constr list * constr list * pos
+  | CIfnp of id * constr list * constr list * pos
+  | CLet of id * id * pos
+  | CLetDeref of id * id * pos
+  | CLetAddPtr of id * id * exp * pos
+  | CLetSubPtr of id * id * exp * pos
+  | CMkArray of id * constr list * simpleTy * pos
+  | CAssignInt of id * pos
+  | CAssignRef of id * id * pos
+  | CAlias of id * id * pos
+  | CAliasDeref of id * id * pos
+  | CAliasAddPtr of id * id * exp * pos
+  | CDeref of id * pos
+  | CApp of id * arg list * pos
+and arg = 
+  | AExp of exp
+  | AId of id
