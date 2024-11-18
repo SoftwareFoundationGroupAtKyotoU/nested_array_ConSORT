09c7b834b611fd017b8ebd7b44f1d5ed67268ae9
fujiwarayuusuke <hujiwara.yuusuke.28n@st.kyoto-u.ac.jp>
2024-11-19 05:41:46 +0900

    mainモジュールとライブラリの接続

--- myproject/bin/main.ml
+++ myproject/bin/main.ml
@@ -1,11 +1,10 @@
 open Myproject.Output
 
-let () = match Array.to_list Sys.argv with
+let () = 
+  match Array.to_list Sys.argv with
     [_; file_name] -> 
     (try 
-      (*ファイルを開いて読み取る*)
-      let input = open_in file_name in
-      ()
+      generate_constrs file_name 5
       (* read_eval_print initial_env input *)
     with Sys_error msg -> Printf.eprintf "Error: %s\n" msg)
   | _ -> Printf.eprintf "予期せぬエラーが発生しました"
