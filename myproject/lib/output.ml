c138a7f151184fc49e7d3005e14b6e6e670847c0
fujiwarayuusuke <hujiwara.yuusuke.28n@st.kyoto-u.ac.jp>
2024-11-20 12:44:38 +0900

    所有権推論アルゴリズムの変数の具体化部分の追加

--- myproject/lib/output.ml
+++ myproject/lib/output.ml
@@ -15,7 +15,7 @@ let rec main_int_sub_declare oc all_cs bool_id fun_num iter =
   if fun_num < 0 then 
     ()
   else
-    (let (var_locations, varown_count, fvs, smtlibs) = all_cs_to_smtlib all_cs false fun_num in
+    (let (var_locations, varown_count, fvs, _) = all_cs_to_smtlib all_cs false fun_num in
     (* out_int.smtに所有権計算に必要なsmtlibでの宣言の書き出し，関数評価中の定数係数の宣言 *)
     print_declare oc var_locations fvs fun_num;
     (* out_int.smtに所有権計算に必要なsmtlibでの宣言の書き出し，関数評価前，評価後の定数係数の宣言 *)
@@ -25,26 +25,41 @@ let rec main_int_sub_declare oc all_cs bool_id fun_num iter =
     (* 次の関数の所有権をsmtlib形式で宣言 *)
     main_int_sub_declare oc all_cs bool_id (fun_num-1) iter)
 
+let rec main_int_sub oc all_cs bool_id flag fun_num iter = 
+  if fun_num < 0 then 
+    ()
+  else
+    (* n番目の関数を表す組，slsは準smtlib形式の制約のリスト，flagは制約の統合の仕方？ *)
+    (let (_, _, fvs, smtlibs) = all_cs_to_smtlib all_cs flag fun_num in
+    (* 制約をassertとしてファイルに書き出し，bool_idは関数print_smtlibsの分岐 *)
+    print_smtlibs oc smtlibs bool_id fvs (-1) iter;
+    (* 関数の制約の間は二行開ける *)
+    output_string oc "\n\n";
+    (* 次の関数の制約出力へ *)
+    main_int_sub oc all_cs bool_id flag (fun_num-1) iter)
+
 (** First phase of the ownershipip inference:
     Generates n_1, ..., n_k and checks the validity of \exists y . phi(n_1, y) /\ ... /\ phi(n_k, y) *)
 let generate_constrs file iter = 
   let oc = open_in file in
-  try
-    let program = Parser.toplevel Lexer.main (Lexing.from_channel oc) in
-    close_in oc;
-    let (fdefs, _) = program in
-    let fun_num = List.length fdefs in 
-    infer_prog_simpleTy program;
-    (* ASTの精緻化 *)
-    let elaborate_program = elaborate_prog program in
-    (* 制約収集 *)
-    let all_constrs = collect_program_own_constraints elaborate_program in 
+  let program = Parser.toplevel Lexer.main (Lexing.from_channel oc) in
+  close_in oc;
+  let (fdefs, _) = program in
+  let fun_num = List.length fdefs in 
+  infer_prog_simpleTy program;
+  (* ASTの精緻化 *)
+  let elaborate_program = elaborate_prog program in
+  (* 制約収集 *)
+  let all_constrs = collect_program_own_constraints elaborate_program in 
 
-    let oc1 = open_out "experiment/out_int.smt2" in
-    (* 所有権計算に必要なsmtlibでの変数宣言の書き出し *)
-    main_int_sub_declare oc1 all_constrs false fun_num iter
-  with
-  | Parse_error (start_p, _) ->
-    printf "Parse error at line %d, column %d\n"
-      start_p.Lexing.pos_lnum
-      (start_p.Lexing.pos_cnum - start_p.Lexing.pos_bol + 1)
+  let oc1 = open_out "experiment/out_int.smt2" in
+  (* 所有権計算に必要なsmtlibでの変数宣言の書き出し *)
+  main_int_sub_declare oc1 all_constrs false fun_num iter;
+  (* 所有権計算に必要なsmtlibでのassert式の書き出しと
+  ヒューリスティクスによるfor all付きの変数の整数値への具体化 *)  
+  main_int_sub oc1 all_constrs false false fun_num iter;
+  (* 充足可能か調べる *)
+  output_string oc1 "(check-sat)\n";
+  (* 充足可能な場合に具体的な値を取得 *)
+  output_string oc1 "(get-model)\n";
+  close_out oc1;
