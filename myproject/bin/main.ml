fefdb388d1060af4bec1b74f22d0eb4a5e6bd844
fujiwarayuusuke <hujiwara.yuusuke.28n@st.kyoto-u.ac.jp>
2024-11-26 16:37:42 +0900

    duneで所有権推論が自動化できるように更新

--- myproject/bin/main.ml
+++ myproject/bin/main.ml
@@ -4,7 +4,30 @@ let () =
   match Array.to_list Sys.argv with
     [_; file_name] -> 
     (try 
-      generate_constrs file_name 5
+      let iter = ref 0 in
+      let continue = ref true in
+      while !continue do
+        generate_constrs file_name !iter;
+        let _ = Sys.command "z3 experiment/out_int.smt2 > experiment/result_int" in
+        (* Printf.printf "%d\n" exit_code; *)
+        main_fv file_name;
+        let _ = Sys.command "z3 experiment/out_fv.smt2 > experiment/result" in
+        let ic = open_in "experiment/result" in
+        (* 最初の行を読み取る *)
+        let first_line = input_line ic in
+        (* ファイルを閉じる *)
+        close_in ic;
+        (* 比較して結果を出力 *)
+        (if first_line = "sat" then 
+          (Printf.printf "iter: %d sat\nownership: sat" !iter;
+          continue := false) 
+        else 
+          Printf.printf "iter: %d unsat \n" !iter );
+        flush stdout;
+        iter := !iter + 1       
+      done;
       (* read_eval_print initial_env input *)
     with Sys_error msg -> Printf.eprintf "Error: %s\n" msg)
+  | [_; file_name; iter] ->
+    generate_constrs file_name (int_of_string iter)
   | _ -> Printf.eprintf "予期せぬエラーが発生しました"
