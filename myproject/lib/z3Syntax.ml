af9b3bb8e937e72315235eece1372fc8dac21adf
fujiwarayuusuke <hujiwara.yuusuke.28n@st.kyoto-u.ac.jp>
2025-04-01 13:02:42 +0900

    z3の結果から該当idのものを探して答えを出力する関数

--- myproject/lib/z3Syntax.ml
+++ myproject/lib/z3Syntax.ml
@@ -47,3 +47,20 @@ and print_z3result_sub oc def =
      output_string oc (string_of_float f2);
      output_string oc (")) :named val" ^ (string_of_int !serial_num) ^ "))\n");
      serial_num := !serial_num + 1)
+
+(* z3の結果から該当idのものを探して答えを出力する関数 *)
+let rec print_z3result_value oc z3_res id = 
+  match z3_res with
+  | [] -> output_string oc id
+  | (id', _,Int i) :: _ when id = id' ->
+    (output_string oc (string_of_int i);)
+  | (id',_,Float f) :: _ when id = id' ->
+    (output_string oc (string_of_float f);)
+  | (id',_,Div (f1,f2))  :: _ when id = id' ->
+    ( output_string oc (" (/ ");
+      output_string oc (string_of_float f1);
+      output_string oc " ";
+      output_string oc (string_of_float f2);
+      output_string oc (")");
+      )
+  | _ :: left -> print_z3result_value oc left id
