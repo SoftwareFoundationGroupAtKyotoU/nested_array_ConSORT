4f9c1d1bb75475252452dce44d0cce8758a20b04
fujiwarayuusuke <hujiwara.yuusuke.28n@st.kyoto-u.ac.jp>
2025-06-11 18:04:56 +0900

    smtlibのmap関数を作成

--- myproject/lib/smtlibSyntax.ml
+++ myproject/lib/smtlibSyntax.ml
@@ -26,3 +26,21 @@ type smtlib =
   | VarPred
   | Ands of smtlib list
   | True
+
+let rec map_smtlib f smtlib =
+  match smtlib with
+  | FV _ | Id _ | IntPred _ | IntVarPred _ | PtrPred _ 
+  | PtrVarPred _ | VarPred _ | True -> smtlib
+  | Or(sl1, sl2) -> Or(map_smtlib f sl1, map_smtlib f sl2)
+  | And(sl1, sl2) -> And(map_smtlib f sl1, map_smtlib f sl2)
+  | Imply(sl1, sl2) -> Imply(map_smtlib f sl1, map_smtlib f sl2)
+  | Not sl -> Not (map_smtlib f sl)
+  | Eq(sl1, sl2) -> Eq(map_smtlib f sl1, map_smtlib f sl2)
+  | Lt(sl1, sl2) -> Lt(map_smtlib f sl1, map_smtlib f sl2)
+  | Gt(sl1, sl2) -> Lt(map_smtlib f sl1, map_smtlib f sl2)
+  | Leq(sl1, sl2) -> Leq(map_smtlib f sl1, map_smtlib f sl2)
+  | Geq(sl1, sl2) -> Geq(map_smtlib f sl1, map_smtlib f sl2)
+  | Add(sl1, sl2) -> Add(map_smtlib f sl1, map_smtlib f sl2)
+  | Sub(sl1, sl2) -> Sub(map_smtlib f sl1, map_smtlib f sl2)
+  | Mul(sl1, sl2) -> Mul(map_smtlib f sl1, map_smtlib f sl2)
+  | Ands sls -> Ands (List.map f sls)
